780b5cb32f13dda01a390ed880554c772efa2d40
Patrick Martin <pux1987@gmail.com>
2019-01-25 21:30:58 -0700

    I just went from empty project to native activity.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,3 +1,9 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+#include <android_native_app_glue.h>
+
+void android_main(struct android_app* state) {
+    __android_log_print(ANDROID_LOG_INFO, "native-activity", "Party Time!");
+}
 
