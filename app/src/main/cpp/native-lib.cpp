f86f5eac8964f9dd94e02c81b6680bc8e3981341
Patrick Martin <pux1987@gmail.com>
2019-01-25 22:24:34 -0700

    Added vulkan_wrapper to the project

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,7 +3,10 @@
 #include <android/log.h>
 #include <android_native_app_glue.h>
 
+#include <vulkan_wrapper.h>
+
 void android_main(struct android_app* state) {
-    __android_log_print(ANDROID_LOG_INFO, "native-activity", "Party Time!");
+    InitVulkan();
+    __android_log_print(ANDROID_LOG_INFO, "native-activity", "Party Time! + Vulkan");
 }
 
